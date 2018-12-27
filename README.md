eea5fc9d0646a35a4035c406e87b1b6fd61d9432
adam.html <adam.html@kakaomobility.com>
2018-12-27 18:21:25 +0900

    https://yts.am/api 연동

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # movieql
-Movie API with GraphQL
+
+## Movie API with GraphQL
+
+**실행 방법**
+
+1. yarn start
+2. localhost:4000
+
+**영화 점수 5점 이상인 것들 최대 3개까지만 가져오기**
+
+```graphql
+query {
+  movies(limit:3, rating:5) {
+    id
+    title
+    rating
+    medium_cover_image
+  }
+}
+```
+
+**영화 하나 고르고 추천 영화까지 함께 받기**
+
+```graphql
+query {
+  movie(id:7893) {
+    id
+    title
+    rating
+    summary
+    description_intro
+    language
+    medium_cover_image
+  }
+  suggestions(id:7893) {
+    title
+    rating
+  }
+}
+```
--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,49 @@
-let movies = [
-  {
-    id: 1,
-    name: 'Star Wars - The new one',
-    score: 1,
-  },
-  {
-    id: 2,
-    name: 'Avengers - The new one',
-    score: 8,
-  },
-  {
-    id: 3,
-    name: 'The Godfather I',
-    score: 99,
-  },
-  {
-    id: 4,
-    name: 'Logan',
-    score: 2,
-  },
-];
+import axios from 'axios';
 
-export const getById = id => {
-  const filteredMovies = movies.filter(movie => id === movie.id);
-  return filteredMovies[0];
-}
+const BASE_URL = 'https://yts.am/api/v2/';
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
-export const getMovies = () => movies;
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating,
+    },
+  });
 
-export const deleteMovie = (id) => {
-  const clearedMovies = movies.filter(movie => movie.id !== id);
-  if (movies.length > clearedMovies.length) {
-    movies = clearedMovies;
-    return true;
-  } else {
-    return false;
-  }
-}
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
+  return movies;
 };
+
+export const getMovie = async (id) => {
+  const {
+    data: {
+      data: { movie },
+    },
+  } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
+
+  return movie;
+};
+
+export const getSuggestions = async (id) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios(MOVIE_SUGGESIONS_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
+
+  return movies;
+}
